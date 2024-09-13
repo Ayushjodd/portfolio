9474586c32e24437081f5801f80f7ad898eabec9
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-13 16:32:35 +0530

    added skills

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Projects = () => {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold ">Projects 🚀</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Projects;
--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,9 +1,51 @@
 import React from "react";
+import { Badge } from "../ui/badge";
 
 const Skills = () => {
+  const skills = {
+    Frontend: ["TypeScript", "React/Next.js", "Tailwind"],
+    Backend: ["Node.js/Express.js", "SQL/NoSQL → Prisma", "WebSockets"],
+    Authentication: [
+      "NextAuth ",
+      " Clerk",
+      " FireBase",
+      " JWT",
+      "zod",
+      "bycryptJS",
+    ],
+    General: [
+      "DataStructures/Algorithms",
+      "Networking/Infrastructure",
+      "Linux",
+      "git/Github",
+    ],
+    "DevOps/Cloud": [
+      "Docker",
+      "Kafka",
+      "Redis → pub/subs",
+      "Prometheus/Grafana",
+      "Microservices",
+    ],
+    Other: ["Touch Typing → 80wpm"],
+  };
+
   return (
     <div>
-      <h1 className="text-2xl font-bold ">Skills ⚙️</h1>
+      <h1 className="text-2xl font-bold">Skills ⚙️</h1>
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        {Object.entries(skills).map(([category, skillsList], index) => (
+          <div key={index}>
+            <h2 className="text-lg font-semibold">{category}</h2>
+            <div className="flex flex-wrap gap-2 mt-2">
+              {skillsList.map((skill, idx) => (
+                <Badge key={idx} className="">
+                  {skill}
+                </Badge>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
