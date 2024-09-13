a6eb9d2e72f13be35e6932fa965d6eb5934f238c
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-13 15:07:39 +0530

    added

---
+++ components/main/AboutMe.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-2xl font-bold">About Me 💀</h1>
+        <p className="text-[#9ca3af] mt-5">
+          Hey there! I'm Ayush, a full-stack developer with a deep passion for
+          building modern web applications. Over the past few years, I’ve honed
+          my skills in **React**, **Node.js**, and **TypeScript**, developing
+          both frontend and backend systems. Currently, I’m diving into the
+          exciting world of **Blockchain development**, working with
+          **Solidity** and exploring **decentralized applications (dApps)**.
+          When I'm not coding, you can find me exploring new technologies,
+          contributing to open-source projects, or reading about the future of
+          **Web3**. I'm always looking for new opportunities to learn, grow, and
+          create impactful solutions in the ever-evolving tech world. Feel free
+          to check out my projects or connect with me!💪
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
