ef6427f7c378b30982fc7b6f0153fd8836d2d479
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-13 16:03:43 +0530

    added more comps

---
+++ components/main/Education.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold ">Education</h1>
+    </div>
+  );
+};
+
+export default Education;
---
+++ components/main/OpenSource.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OpenSource = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Open Source Contribution 🤝</h1>
+      <div className="text-[#9ca3af]">Comming real soon....😥</div>
+    </div>
+  );
+};
+
+export default OpenSource;
